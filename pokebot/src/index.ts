b26b5c08ad3959f2d0eed1611659f7319db6293b
Arthur-Jacobina <email27arthur@gmail.com>
2024-09-06 20:16:02 -0300

    add basic commands and context handlign

--- pokebot/src/index.ts
+++ pokebot/src/index.ts
@@ -1,9 +1,24 @@
 import { run, HandlerContext } from "@xmtp/message-kit";
 
 run(async (context: HandlerContext) => {
-  // Get the message and the address from the sender
-  const { content, sender } = context.message;
-
-  // To reply, just call `reply` on the HandlerContext.
-  await context.reply(`https://edpon-frames.vercel.app/api`);
+  const { message: { typeId } } = context;
+  console.log(typeId);
+  if (typeId === "text" || typeId === "reply") {
+    const { message: { content: { content: text } } } = context;
+    if (text === "/battle") {
+      await context.reply(`https://edpon-frames.vercel.app/api`);
+    } 
+  } else if (typeId === "reaction") {
+    const {
+      message: {
+        content: { content: emoji, action },
+      },
+    } = context;
+    if (action === "added" && (emoji === "🔂" || emoji === "🔁")) {
+      await context.send("https://edpon-frames.vercel.app/api");
+    }
+  }
+  // send just sends the frame in the chat 
+  // when pinging the user is necessary consider using reply 
+  // await context.send(`https://edpon-frames.vercel.app/api`);
 });
