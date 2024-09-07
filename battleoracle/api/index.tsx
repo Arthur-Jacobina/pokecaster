c5dd2527d5a9b9ecab3af73184173e2b5fed257e
Arthur-Jacobina <email27arthur@gmail.com>
2024-09-07 13:49:31 -0300

    add dynamic image generation for battle list

--- battleoracle/api/index.tsx
+++ battleoracle/api/index.tsx
@@ -1,9 +1,12 @@
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Button, Frog } from "frog";
 import { devtools } from "frog/dev";
-import { Context, Next } from 'hono';
+import { Context, Next } from 'hono'
+import { handle } from 'frog/vercel';
+import { serve } from '@hono/node-server';;
 import { validateFramesPost } from "@xmtp/frames-validator";
-import { getBattleById, getBattleIdByStatus } from "../lib/database.js";
+import { generateBattleList } from "../image-generation/generator.js";
+// import { getBattleById, getBattleIdByStatus } from "../lib/database.js";
 
 const title = 'battle-oracle'
 
@@ -51,27 +54,58 @@ app.use("/*", serveStatic({ root: "./public" }));
   // console.log("verifiedWalletAddress", verifiedWalletAddress);
 app.frame("/", (c) => {
   return c.res({
-    image: `${42}`,
+    title,
+    image: `/public/bocover.png`,
+    imageAspectRatio: '1:1',
     intents: [
-      <Button value="apples">⬅️</Button>,
-      <Button value="oranges">✅</Button>,
-      <Button value="bananas">➡️</Button>,
+      <Button action={`/0`}>Battles</Button>,
+      <Button action={`/`}>Share</Button>,
     ],
   });
 });
 
 app.frame("/:id", async (c) => {
   const id = Number(c.req.param('id'));
-  const waitingBattles = await getBattleIdByStatus('waiting');
-  const battle = getBattleById(waitingBattles[id]);
+  // const waitingBattles = await getBattleIdByStatus('waiting');
+  // const battle = await getBattleById(waitingBattles[id]);
+  // const battlePokemons = battle.maker_pokemons;
+  const battlePokemons = [1,4,25];
   return c.res({
-    image: `${42}`,
+    title,
+    image: `/image/battlelist/${battlePokemons[0]}/${battlePokemons[1]}/${battlePokemons[2]}`,
+    imageAspectRatio: '1:1',
     intents: [
       <Button action={`/${id+1}`}>⬅️</Button>,
-      <Button action={``}>✅</Button>,
+      <Button action={`/`}>✅</Button>,
       <Button action={`/${id-1}`}>➡️</Button>,
     ],
   });
 });
 
-devtools(app, { serveStatic });
+app.hono.get('/image/battlelist/:p1/:p2/:p3', async (c) => {
+  try {
+    const p1 = Number(c.req.param('p1'));
+    const p2 = Number(c.req.param('p2'));
+    const p3 = Number(c.req.param('p3'));
+
+    const image = await generateBattleList([p1,p2,p3]);
+
+    return c.newResponse(image, 200, {
+      'Content-Type': 'image/png',
+      'Cache-Control': 'max-age=0', 
+    });
+  } catch (error) {
+    console.error("Error generating image:", error);
+    return c.newResponse("Error generating image", 500);
+  }
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  devtools(app, { serveStatic });
+}
+
+serve({ fetch: app.fetch, port: Number(process.env.PORT) || 5173 });
+console.log(`Server started: ${new Date()} `);
+
+export const GET = handle(app)
+export const POST = handle(app)
---
+++ battleoracle/image-generation/generator.tsx
@@ -0,0 +1,42 @@
+import sharp from 'sharp'
+
+export const generateBattleList = async (
+    pokemonIds: number[],
+  ) => {
+    try {
+    const ComponentsArray = [];
+    
+    const baseImageBuffer = await sharp('./public/battle-oracle-base.png')
+    .resize(600, 600)
+    .png()
+    .toBuffer();
+  
+    const pokemon1ImageBuffer = await sharp(`./public/pokemons/${pokemonIds[0]}.png`)
+    .resize(86, 86)
+    .png()
+    .toBuffer();
+  
+    const pokemon2ImageBuffer = await sharp(`./public/pokemons/${pokemonIds[1]}.png`)
+    .resize(86, 86)
+    .png()
+    .toBuffer();
+  
+    const pokemon3ImageBuffer = await sharp(`./public/pokemons/${pokemonIds[2]}.png`)
+    .resize(86, 86)
+    .png()
+    .toBuffer();
+  
+    ComponentsArray.push({input: pokemon1ImageBuffer, top: 60, left: 122});
+    ComponentsArray.push({input: pokemon2ImageBuffer, top: 60, left: 258});
+    ComponentsArray.push({input: pokemon3ImageBuffer, top: 60, left: 397});
+    const finalImage = await sharp(baseImageBuffer)
+    .composite(ComponentsArray)
+    .png()
+    .toBuffer();
+  
+    return finalImage;
+    } catch(error) {
+        console.error("Error during battle checkout generation:", error);
+        throw error;
+    }
+  }
