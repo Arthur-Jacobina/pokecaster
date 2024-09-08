c78b72493a317a99b8b50dcc6577136300b860cf
Arthur-Jacobina <email27arthur@gmail.com>
2024-09-08 02:05:56 -0300

    fix url

--- pokebot/src/lib/database.ts
+++ pokebot/src/lib/database.ts
@@ -4,7 +4,7 @@ export const getFid = async (username: string) => {
   // try to get fid from database
   // if fid is not found, need to redirect user to the /register frame
 
-  const response = await fetch(`ethonline24-production.up.railway.app/api/converse-user/${username}`);
+  const response = await fetch(`http://ethonline24-production.up.railway.app/api/converse-user/${username}`);
 
   if(response.status == 500) {
     throw new Error("Internal Server Error");
